59b8a04579e1b368d3dda9473e9d20620a8938c3
kraskaGM <martingrosberg@gmail.com>
2020-02-11 12:59:07 +0200

    Create myJsFunctions.js

---
+++ UserInterfacev1.4/myJsFunctions.js
@@ -0,0 +1,209 @@
+//Variables
+var url = "ws://192.168.1.1:1337/";
+var output;
+var button;
+var canvas;
+var flow=0;
+var pump=0;
+
+function changeOutput() {
+	onPress();
+	counter();
+}
+
+
+function counter()
+{
+	var x = document.getElementById("myCheck").checked;
+	var y = document.getElementById("myRange2").value;
+	if (x==true) 
+	{		  
+		var counter = y;	
+		setInterval(function()
+		{
+			counter--;
+			x = document.getElementById("myCheck").checked;				
+			if (counter > 0 & x==true)
+			{
+				id = document.getElementById("count");
+				id.innerHTML=counter;
+			}
+			else if (counter == 0 & x==true)
+			{
+				id.innerHTML="COMPLETE";
+			}
+			else if (x==false) 
+			{
+			id.innerHTML="Stopped";
+				counter = document.getElementById("myRange2").value;
+			}
+		},1000);
+	}
+	
+}
+
+function sendtime(postime) {
+	var someUrl  = "/postflow";
+	$.ajax({
+		url: someUrl,
+		dataType: "text",
+		success: function(response) {flow = parseInt(response);},
+		timeout: 2000
+		}
+	)
+}		
+
+
+function Setflow(postflow) {
+	var FlowRate =document.getElementById("myRange").value;
+	var someUrl = "/postflow/update?value=" + FlowRate;
+	$.ajax({url: someUrl,dataType: "text",success: function(response) {flow = parseInt(response);}})}
+	
+
+function sendtime(postime) {
+	var someUrl  = "/postime";
+	$.ajax({
+		url: someUrl,
+		dataType: "text",
+		success: function(response) {pump = parseInt(response);},
+		timeout: 2000
+		}
+	)
+}
+function Settime(postime) {
+	var Pumptime = document.getElementById("myRange2").value;	
+	var someUrl = "/postime/update?value=" + Pumptime;
+	$.ajax({url: someUrl,dataType: "text",success: function(response) {pump = parseInt(response);}})}
+
+	
+
+
+// This is called when the page finishes loading
+function init() {
+ 
+    // Assign page elements to variables
+    button = document.getElementById("toggleButton");
+    output = document.getElementById("output");
+    canvas = document.getElementById("led");
+    
+    
+    // Connect to WebSocket server
+    wsConnect(url);
+}
+
+
+// Call this to connect to the WebSocket server
+function wsConnect(url) {
+    
+    // Connect to WebSocket server
+    websocket = new WebSocket(url);
+    
+    // Assign callbacks
+    websocket.onopen = function(evt) { onOpen(evt) };
+    websocket.onclose = function(evt) { onClose(evt) };
+    websocket.onmessage = function(evt) { onMessage(evt) };
+    websocket.onerror = function(evt) { onError(evt) };
+}
+
+// Called when a WebSocket connection is established with the server
+function onOpen(evt) {
+ 
+    // Log connection state
+    console.log("Connected");
+    
+    // Enable button
+    button.disabled = false;
+    
+    // Get the current state of the LED
+    doSend("getLEDState");
+}
+
+
+// Called when the WebSocket connection is closed
+function onClose(evt) {
+ 
+    // Log disconnection state
+    console.log("Disconnected");
+    
+    // Disable button
+    button.disabled = true;
+    
+    // Try to reconnect after a few seconds
+    setTimeout(function() { wsConnect(url) }, 2000);
+}
+ 
+// Called when a message is received from the server
+function onMessage(evt) {
+ 
+    // Print out our received message
+    console.log("Received: " + evt.data);
+    
+    // Update circle graphic with LED state
+    switch(evt.data) {
+        case "0":
+            console.log("LED is off");
+            context.fillStyle = "black";
+            context.fill();
+            break;
+        case "1":
+            console.log("LED is on");
+            context.fillStyle = "red";
+            context.fill();
+            break;
+        default:
+            break;
+    }
+}
+ 
+// Called when a WebSocket error occurs
+function onError(evt) {
+    console.log("ERROR: " + evt.data);
+}
+ 
+// Sends a message to the server (and prints it to the console)
+function doSend(message) {
+    console.log("Sending: " + message);
+    websocket.send(message);
+}
+ 
+// Called whenever the HTML button is pressed
+function onPress() {
+    doSend("toggleLED");
+    doSend("getLEDState");
+}
+ 
+// Call the init function as soon as the page loads
+window.addEventListener("load", init, false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
