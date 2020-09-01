9385f86b65f028ce015cd5a0a0c493da11b89517
kraskaGM <martingrosberg@gmail.com>
2020-09-01 15:28:31 +0300

    Update MACaddress.cpp

--- ESP2ESPCOM_v1.2/MACaddress.cpp
+++ ESP2ESPCOM_v1.2/MACaddress.cpp
@@ -1,47 +1,130 @@
 /******************************************************************************
-
                               Online C++ Compiler.
                Code, Compile, Run and Debug C++ program online.
 Write your code in this editor and press "Run" button to compile and execute it.
-
 *******************************************************************************/
 
 #include <iostream>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 using namespace std;
 
-int main()
+//Getting the MAC address characters from the input
+char** MACextraction(char input[])
 {
-    cout<<"Hello World \n";
-    //char colour[4][10] = { "Blue", "Red", "Orange", "Yellow" }; 
-    char array[6][3];
-    //{ "4C", "11", "AE", "EA", "EE", "B4" };
-    char input[]="[\"0x4C\",\"0x11\",\"0xAE\",\"0xEA\",\"0xEE\",\"0xB4\"]";
-    //uint8_t Output[]=[0x4C, 0x11, 0xAE, 0xEA, 0xEE, 0xB4];
-    //int ArraySize = sizeof(Input);
-    int size = sizeof(input);
-    cout<<input<<"\n";
-    cout <<"Size of tthe array is: ";
-    cout<<size<<"\n";
+    int size = strlen(input);
+    cout <<"MACextraction Input array size is: \n";
+    cout<< size<<"\n";
+    char** result = new char*[6];
+    for (int i=0; i<6;++i)
+    {
+        result[i]=new char[3];
+    }
+    cout <<"MACextraction array generation  is: \n";
     int j=0;
-    int k=0;
-
     for(int i=0; i<size;++i)
     {   
+        int k=0;
         char comp = 'x';
         if(input[i]==comp)
         {
-            array[j][k]=input[i+1];
+            result[j][k]=input[i+1];
             cout<<input[i+1];
-            array[j][k+1]=input[i+2];
+            result[j][k+1]=input[i+2];
             cout<<input[i+2];
             cout<<" \n";
             ++j;
         }
-        
+    }  
+    return result;
+}
+
+//Adding character together
+char** CharConc(char* input[])
+{
+    //Creating a new array to restore the conversion results.
+    char** result = new char*[6];
+    for (int i=0; i<6;++i)
+    {
+        result[i]=new char[3];
     }
+    //Converting
+    cout <<"Converted char array in int is: \n";
+    for(int i=0; i<6;++i)
+    {   
+        result[i][0]=(input[i][0]);
+        strncat(result[i], &input[i][1], 1);
+        cout<<result[i];
+        cout<<" \n";
+    }  
+    return result; 
+}
+
+int hex2decimal(char hex[])
+{
+    long long decimal, place;
+    int i = 0, val, len;
+
+    decimal = 0;
+    place = 1;
+
+    /* Input hexadecimal number from user */
+    //printf("Enter any hexadecimal number: ");
+    //gets(hex);
+
+    /* Find the length of total number of hex digit */
+    len = strlen(hex);
+    len--;
+
+    /*
+     * Iterate over each hex digit
+     */
+    for(i=0; hex[i]!='\0'; i++)
+    {
+ 
+        /* Find the decimal representation of hex[i] */
+        if(hex[i]>='0' && hex[i]<='9')
+        {
+            val = hex[i] - 48;
+        }
+        else if(hex[i]>='a' && hex[i]<='f')
+        {
+            val = hex[i] - 97 + 10;
+        }
+        else if(hex[i]>='A' && hex[i]<='F')
+        {
+            val = hex[i] - 65 + 10;
+        }
+
+        decimal += val * pow(16, len);
+        len--;
+    }
+    cout<<"Hexadecimal number = is: "<< hex<<" \n";
+    cout<<"Decimal number is: "<< decimal<<" \n";
+    //printf("Hexadecimal number = %s\n", hex);
+    //printf("Decimal number = %lld", decimal);
+    return decimal;
+}
+int main()
+{
+    int val =hex2decimal("4C");
+    cout<<val<<"\n";    
+    char** array;
+    char input[]="[\"0x4C\",\"0x11\",\"0xAE\",\"0xEA\",\"0xEE\",\"0xB4\"]";
+    int size = sizeof(input);
+    cout <<"Outside the function Input array size is: \n";
+    cout<< size<<"\n";
+    array=MACextraction(input);
+    char** converted;
+    converted = CharConc(array);
+    for (int i=0;i<6;++i)
+    {
+        hex2decimal(converted[i]);
+    }
+
     int s=0;
     cout <<"Array is: \n";
     for (int i=0;i<6;++i)
@@ -50,51 +133,5 @@ int main()
          cout<<array[i][s+1];
          cout<<" \n";
     }
-
-    int mac1=(array[0][0]);//array[0][1];
-    int ma1_2=(array[0][1]);
-    //uint8_t ma1_2 =0x43;
-    cout <<"MAC 2nd char is: \n";
-    cout<< ma1_2<<"\n";
-
-    cout <<"MAC: \n";
-    cout<< mac1<<"\n";
-    //mac1= mac1<<4;
-    cout <<"Shifted MAC is: \n";
-    cout<< mac1<<"\n";
-    
-    mac1|=ma1_2;
-    mac1=mac1^0x38;
-    cout <<"Merged MAC is: \n";
-    cout<< mac1<<"\n";   
-    
-    unsigned int test =4;
-    unsigned int test_2=12;
-    
-    cout <<"test is: \n";
-    cout << test;
-    
-    cout<<" \n";
-    test = test <<4;
-    test = test;
-    cout <<"Shifted test is: \n";
-    cout << test<<"\n";
-    
-    test = test|test_2;
-    cout <<"Merged MAC is: \n";
-    cout<< test<<"\n";  
-    
-    uint8_t mac2=array[1][0];//&array[1][1];
-    uint8_t mac3=array[2][0];//&array[2][1];
-    uint8_t mac4=array[3][0];//&array[3][1];
-    uint8_t mac5=array[4][0];//&array[4][1];
-    uint8_t mac6=array[5][0];//&array[5][1];
-    /*cout<< test<<"\n";*/
-    /*cout<< mac1<<"\n";
-    cout<< mac2<<"\n";
-    cout<< mac3<<"\n";
-    cout<< mac4<<"\n";
-    cout<< mac5<<"\n";
-    cout<< mac6<<"\n";*/
     return 0;
 }
